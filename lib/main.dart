4824e55ab0ac886b6fd7268097f1f537f702c13e
Iampato <858wpwaweru@gmail.com>
2019-07-17 20:57:39 +0300

    Added Homepage

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.blue
       ),
       home: HomePage();
+      
     );
   }
 }
@@ -18,6 +19,42 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Sc
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(" ALC Phase 1"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Text("Welcome to A.L.C 4.0",
+              style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold)
+          ),
+          SizedBox(height: 10),
+          RaisedButton(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5)
+            ),
+            color: Colors.orange,
+            child: Text("About ALC"),
+            onPressed: (){
+              //TODO
+            },
+          ),
+          SizedBox(height: 5),
+          RaisedButton(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5)
+            ),
+            color: Colors.orange,
+            child: Text("My Profile"),
+            onPressed: (){
+              MaterialPageRoute(
+                  builder: (BuildContext context) {
+                    return ;//TODO
+                  });
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
