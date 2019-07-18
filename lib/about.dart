fc1d071671ab7d37b4be04537d95eacbce5310db
Iampato <858wpwaweru@gmail.com>
2019-07-18 13:27:35 +0300

    Fixed a bug on text font suze

---
+++ lib/about.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+const kAndroidUserAgent =
+    'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Mobile Safari/537.36';
+
+class AboutPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return WebviewScaffold(
+      appBar: AppBar(
+        title: Text("About ALC"),
+      ),
+      withJavascript: true,
+      withZoom: true,
+      userAgent: kAndroidUserAgent,
+      withLocalStorage: true,
+      url: "https://andela.com/alc/",
+      initialChild: Container(
+        alignment: Alignment.center,
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
