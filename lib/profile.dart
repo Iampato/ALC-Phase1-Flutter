6a3385ee997826809cfde79520dd90fe06a3bd44
Iampato <858wpwaweru@gmail.com>
2019-07-18 13:27:05 +0300

    Added about page

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,23 +1,50 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
+
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  
   @override
   Widget build(BuildContext context) {
+    TextStyle textstyle = TextStyle(fontSize: 20,);
+
     return Scaffold(
       appBar: AppBar(
         title: Text("My Profile")
       ),
       body: new SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(top: 15,bottom: 10),
-              child: CircleAvatar(
-                child: Image.asset("profile.jpg"),
+        physics: ClampingScrollPhysics(),
+        child: Align(
+          alignment: Alignment.center,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+           // mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 15,bottom: 20),
+                child: CircleAvatar(
+                  radius: 120,
+                 
+                  backgroundImage: AssetImage("profile.jpg"),
+                ),
               ),
-            )
-          ],
+              Text("Track:  Android",style: textstyle,),
+              SizedBox(height: 1),
+              Text("Name:  Patrick Waweru",style: textstyle,),
+              SizedBox(height: 1),
+              Text("Country:  Kenya",style: textstyle,),
+              SizedBox(height: 1),
+              Text("Github:  @Iampato",style: textstyle,),
+               SizedBox(height: 1),
+              Text("Slack Username:  @Iampato",style: textstyle,),
+             
+            ],
+          ),
         ),
       ),
     );
